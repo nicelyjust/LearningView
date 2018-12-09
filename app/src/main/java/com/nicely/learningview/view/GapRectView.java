6f89bd353cc3e039ad711c3e1fb1cdb8421c53dd
nicelyjust <nicely_email@yeah.net>
2018-12-09 19:53:11 +0800

    矩形分割图基本搞定；

--- app/src/main/java/com/nicely/learningview/view/GapRectView.java
+++ app/src/main/java/com/nicely/learningview/view/GapRectView.java
@@ -13,12 +13,15 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+
+import com.nicely.learningview.util.TDevice;
 
 public class GapRectView extends View {
     private static final int DEFAULT_WIDTH = 400;
@@ -53,6 +56,8 @@ public class GapRectView extends View {
     private Paint mTrianglePaint;
     private Shader mShader;
     private float mLineX;
+    private int mStartY;
+    private Path mTriPath;
 
     public GapRectView(Context context) {
         this(context, null);
@@ -69,6 +74,8 @@ public class GapRectView extends View {
 
     private void initPaint() {
 
+        gapWidth = TDevice.dip2px(getContext(), 3);
+
         mRectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mRectPaint.setStyle(Paint.Style.FILL);
 
@@ -81,14 +88,20 @@ public class GapRectView extends View {
         mSplitPaint.setStyle(Paint.Style.FILL);
         mSplitPaint.setColor(spiltColor);
         mSplitPaint.setStrokeCap(Paint.Cap.ROUND);
-        mSplitPaint.setStrokeWidth(gapWidth / 2);
+        mSplitPaint.setStrokeWidth(TDevice.dip2px(getContext(), 2));
 
         mTopTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mTopTextPaint.setTextSize(20);
+        mTopTextPaint.setTextSize(TDevice.dip2px(getContext(), 30));
         mTopTextPaint.setAntiAlias(true);
         mTopTextPaint.setColor(Color.BLACK);
 
         mTopTextPaint.getTextBounds(value + "", 0, (value + "").length(), topTextBound);
+
+        mTriPath = new Path();
+        mTrianglePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTrianglePaint.setStyle(Paint.Style.STROKE);
+        mTrianglePaint.setStrokeWidth(TDevice.dip2px(getContext(), 5));
+
     }
 
     @Override
@@ -102,9 +115,15 @@ public class GapRectView extends View {
                 && measureHeightMode == MeasureSpec.AT_MOST) {
             setMeasuredDimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         } else if (measureWidthMode == MeasureSpec.AT_MOST) {
+            //height是精确的 需要算出文字 + 间距
+            measureHeightSize += topTextBound.height() + 20;
             setMeasuredDimension(DEFAULT_WIDTH, measureHeightSize);
         } else if (measureHeightMode == MeasureSpec.AT_MOST) {
             setMeasuredDimension(measureWidthSize, DEFAULT_HEIGHT);
+        } else if (measureWidthMode == MeasureSpec.EXACTLY
+                && measureHeightMode == MeasureSpec.EXACTLY) {
+            measureHeightSize += topTextBound.height() + 20;
+            setMeasuredDimension(measureWidthSize, measureHeightSize);
         }
     }
 
@@ -115,11 +134,12 @@ public class GapRectView extends View {
         if (mShader == null)
             mShader = new LinearGradient(0, 0, REAl_WIDTH, REAl_HEIGHT, 0xff2f4e7d, 0xff408cfb, Shader.TileMode.CLAMP);
         mRectPaint.setShader(mShader);
-
+        LinearGradient linearGradient = new LinearGradient(0, 10, 0 , topTextBound.height(), 0xff2f4e7d, 0xff408cfb, Shader.TileMode.CLAMP);
+        mTrianglePaint.setShader(linearGradient);
         rectWidth = (REAl_WIDTH - (COUNT - 1) * gapWidth) / COUNT;
         // TODO: 2018/12/8 存在画在黑色区域的情况
         mLineX = value / 100f * REAl_WIDTH;
-
+        mStartY = 15 + topTextBound.height();
     }
 
     @Override
@@ -128,18 +148,23 @@ public class GapRectView extends View {
         /*
          * 画整体矩形 ,上下偏移 5
          */
-        canvas.drawRect(0, 5, REAl_WIDTH, REAl_HEIGHT - 5, mRectPaint);
+        canvas.drawRect(0, mStartY, REAl_WIDTH, REAl_HEIGHT - 5, mRectPaint);
         /*
          * 画 gap
          */
         int size = COUNT - 1;
         for (int i = 1; i <= size; i++) {
-            canvas.drawLine(rectWidth * i + gapWidth * (i - 1), 5, rectWidth * i + gapWidth * (i - 1), REAl_HEIGHT - 5, mGapPaint);
+            canvas.drawLine(rectWidth * i + gapWidth * (i - 1), mStartY, rectWidth * i + gapWidth * (i - 1), REAl_HEIGHT - 5, mGapPaint);
         }
         /*
          * 画当前刻度
          */
-        canvas.drawLine(mLineX , 0 , mLineX , REAl_HEIGHT ,mSplitPaint);
-
+        canvas.drawLine(mLineX, mStartY, mLineX, REAl_HEIGHT, mSplitPaint);
+        canvas.drawText(String.valueOf(value), mLineX - topTextBound.width() / 2, topTextBound.height(), mTopTextPaint);
+        float triX = mLineX + topTextBound.width() / 2 + 25;
+        mTriPath.moveTo(triX, topTextBound.height());
+        mTriPath.lineTo(triX + TDevice.dip2px(getContext(), 11),20);
+        mTriPath.lineTo(triX + TDevice.dip2px(getContext(), 22) , topTextBound.height());
+        canvas.drawPath(mTriPath ,mTrianglePaint);
     }
 }
